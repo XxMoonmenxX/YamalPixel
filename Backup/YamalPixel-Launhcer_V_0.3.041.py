63d2be12282fe8503c498d35390815490e8cc8be
Moonmen <tolikdobrozorov@gmail.com>
2025-10-14 13:00:49 +0500

    Поменял моды в сборке:)

--- Backup/YamalPixel-Launhcer_V_0.3.041.py
+++ Backup/YamalPixel-Launhcer_V_0.3.041.py
@@ -18,7 +18,7 @@ import logging
 from pypresence import Presence
 from pathlib import Path
 
-CURRENT_VERSION = "0.3.04" #обновление
+CURRENT_VERSION = "0.3.041" #обновление
 logging.basicConfig(filename='launcher.log', level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
