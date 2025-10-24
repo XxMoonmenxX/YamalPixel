be016a2c51b362751bc348f837d0a5b9656cea49
Moonmen <tolikdobrozorov@gmail.com>
2025-10-24 17:22:02 +0500

    Исправление всего в этом мире... я устал...

--- Backup/YamalPixel-Launcher_V_0.4.633.py
+++ Backup/YamalPixel-Launcher_V_0.4.633.py
@@ -29,7 +29,7 @@ from pathlib import Path
 
 
 #Пишется при помощи DeepSeek, каждый может сделать тоже самое хоть немного зная python!!!
-CURRENT_VERSION = "0.4.632" #обновление
+CURRENT_VERSION = "0.4.633" #обновление
 logging.basicConfig(filename='launcher.log', level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
