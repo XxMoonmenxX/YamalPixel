77f28dbd282855280c5ec25c87f100668f2701c8
Moonmen <tolikdobrozorov@gmail.com>
2025-11-06 19:15:45 +0500

    Изменил версию

--- Backup/YamalPixel-Launcher_V_0.6.61.py
+++ Backup/YamalPixel-Launcher_V_0.6.61.py
@@ -189,7 +189,7 @@ def old_repair_with_ui():
 
 
 #Пишется при помощи DeepSeek, каждый может сделать то же самое хоть немного зная python!!!
-CURRENT_VERSION = "0.6.6" #обновление
+CURRENT_VERSION = "0.6.61" #обновление
 logging.basicConfig(filename='launcher.log', level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
